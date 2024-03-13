65cc92dc6a6b3dd05731b1e86f287d2d1cad8a65
Miurionochek <ivanfedoniukwork@gmail.com>
2024-03-13 09:27:39 +0200

    Add statistics of packed items

--- far-away/src/App.jsx
+++ far-away/src/App.jsx
@@ -36,7 +36,7 @@ export default function App() {
         />
       </main>
       <footer className="stats">
-        <Stats />
+        <Stats list={items} />
       </footer>
     </div>
   );
@@ -128,8 +128,23 @@ function PackItem({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ list }) {
+  if (!list.length) {
+    return <em>Start adding some items to your packing list 🚀!</em>;
+  }
+  const numOfListItems = list.length;
+  const numOfPackedItems = list.filter((item) => item.packed).length;
+  const percentage = Math.round((numOfPackedItems / numOfListItems) * 100);
   return (
-    <em>🎒You have X items in your list, and you already have packed Y%</em>
+    <>
+      {percentage !== 100 ? (
+        <em>
+          🎒You have {numOfListItems} items in your list, and you have already
+          packed {numOfPackedItems} ({percentage}%)
+        </em>
+      ) : (
+        <em>You got everything! Ready to go ✈</em>
+      )}
+    </>
   );
 }
